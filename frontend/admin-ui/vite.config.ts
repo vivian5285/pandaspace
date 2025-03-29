d0aacd90f1925db98c091930a930e8d5389e135e
vivian5285 <wangjiali240@gmail.com>
2025-03-29 14:55:44 +0800

    f

--- frontend/admin-ui/vite.config.ts
+++ frontend/admin-ui/vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@pages': path.resolve(__dirname, './src/pages')
+    }
+  },
   server: {
     port: 4001,
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
   },
 }); 
